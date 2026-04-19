094079ff5b6efbc9fe4937d5573eea1e7311b056
ducthinhn <ducthinh20062005@gmail.com>
2026-04-19 21:04:11 +0700

    update

--- backend/src/main/java/com/cinemaebooking/backend/seat/infrastructure/mapper/seatType/SeatTypeMapper.java
+++ backend/src/main/java/com/cinemaebooking/backend/seat/infrastructure/mapper/seatType/SeatTypeMapper.java
@@ -1,6 +1,6 @@
 package com.cinemaebooking.backend.seat.infrastructure.mapper.seatType;
 
-import com.cinemaebooking.backend.infrastructure.persistence.mapper.BaseMapper;
+import com.cinemaebooking.backend.infrastructure.mapper.BaseMapper;
 import com.cinemaebooking.backend.seat.domain.model.seatType.SeatType;
 import com.cinemaebooking.backend.seat.infrastructure.persistence.entity.SeatTypeJpaEntity;
 
