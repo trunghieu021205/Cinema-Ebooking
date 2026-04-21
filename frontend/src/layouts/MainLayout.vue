9421d63dfb9a9a32d447c8dc58347e13fc752f4d
Trung Hieu <trunghieu021205@gmail.com>
2026-04-21 13:01:18 +0700

    refactor(frontend): simplify frontend architecture

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+
+    <main class="flex-1 container mx-auto px-4">
+      <router-view />
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/common/Navbar.vue'
+import Footer from '@/components/common/Footer.vue'
+</script>
