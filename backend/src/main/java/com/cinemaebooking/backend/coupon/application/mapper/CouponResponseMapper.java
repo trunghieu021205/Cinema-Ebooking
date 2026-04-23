d3a9a395ab6af56009f24a6b736933c884d3a3b0
Dung190305 <nguoigay102@gmai.coms>
2026-04-23 23:24:01 +0700

    feat(coupon-mapper): thêm chức năng cho mapper

---
+++ backend/src/main/java/com/cinemaebooking/backend/coupon/application/mapper/CouponResponseMapper.java
@@ -0,0 +1,27 @@
+package com.cinemaebooking.backend.coupon.application.mapper;
+
+import com.cinemaebooking.backend.coupon.application.dto.CouponResponse;
+import com.cinemaebooking.backend.coupon.domain.model.Coupon;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CouponResponseMapper {
+
+    public CouponResponse toResponse(Coupon coupon) {
+        if (coupon == null) return null;
+
+        return new CouponResponse(
+                coupon.getId() != null ? coupon.getId().getValue() : null,
+                coupon.getCode(),
+                coupon.getType(),
+                coupon.getValue(),
+                coupon.getUsageLimit(),
+                coupon.getPerUserUsage(),
+                coupon.getPointsToRedeem(),
+                coupon.getMinimumBookingValue(),
+                coupon.getMaximumDiscountAmount(),
+                coupon.getStartDate(),
+                coupon.getEndDate()
+        );
+    }
+}
