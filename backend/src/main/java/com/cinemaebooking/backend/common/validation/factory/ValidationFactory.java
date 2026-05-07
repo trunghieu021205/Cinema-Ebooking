837b05d92fb5ea02fa2e69d521cec7474b1cc534
ducthinhn <ducthinh20062005@gmail.com>
2026-05-07 09:39:10 +0700

    feat(booking): add BookingValidationProfile

--- backend/src/main/java/com/cinemaebooking/backend/common/validation/factory/ValidationFactory.java
+++ backend/src/main/java/com/cinemaebooking/backend/common/validation/factory/ValidationFactory.java
@@ -29,4 +29,5 @@ public final class ValidationFactory {
     public static CouponValidationProfile coupon() {return CouponValidationProfile.INSTANCE;}
     public static ComboValidationProfile combo() {return ComboValidationProfile.INSTANCE;}
     public static ShowtimeValidationProfile showtime(){ return ShowtimeValidationProfile.INSTANCE;}
+    public static BookingValidationProfile booking(){ return BookingValidationProfile.INSTANCE;}
 }
