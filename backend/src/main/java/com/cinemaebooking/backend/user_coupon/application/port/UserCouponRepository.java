472abae19137c9f57c6bffd6aef183b1b4b4f151
Dung190305 <nguoigay102@gmai.coms>
2026-05-08 15:09:42 +0700

    feature(user_coupon - application) : sửa chức năng port UserCouponRepository

--- backend/src/main/java/com/cinemaebooking/backend/user_coupon/application/port/UserCouponRepository.java
+++ backend/src/main/java/com/cinemaebooking/backend/user_coupon/application/port/UserCouponRepository.java
@@ -5,12 +5,19 @@ import com.cinemaebooking.backend.user_coupon.domain.valueobject.UserCouponId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserCouponRepository {
     UserCoupon create(UserCoupon userCoupon);
     UserCoupon update(UserCoupon userCoupon);
     Optional<UserCoupon> findById(UserCouponId id);
+    Optional<UserCoupon> findByUserIdAndCouponId(Long userId, Long couponId);
     Page<UserCoupon> findByUserId(Long userId, Pageable pageable);
+    Page<UserCoupon> findByUserIdAndStatus(Long userId, String status, Pageable pageable);
+    List<UserCoupon> findAvailableExpiredBefore(LocalDateTime dateTime);
     boolean existsByUserIdAndCouponId(Long userId, Long couponId);
+    boolean existsById(UserCouponId id);
+    void deleteById(UserCouponId id);
 }
